72f8500228dd443e272c232a802e134169771a44
Trevan Young <thelastuchiha@pop-os.localdomain>
2025-08-08 16:09:29 -0400

    changed favicon and added book cover

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./App.css";
+import bookCover from "./book-cover.png"; // adjust path if using /assets
 
 function App() {
   return (
     <div className="App">
       <h1>The Autistic Passport</h1>
+
+      {/* Cover image */}
+      <img
+        src={bookCover}
+        alt="The Autistic Passport Cover"
+        className="book-cover"
+      />
+
       <p>Amazon Book Link (coming soon)</p>
       <button className="download-btn">Download Free Ebook (coming soon)</button>
     </div>
